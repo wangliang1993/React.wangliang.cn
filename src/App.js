9df42bf3a21d4a5c4dacdf78b7a11ecb23705120
liang wang <liangwang@liangdeMacBook-Pro.local>
2019-05-19 22:40:14 +0800

    jenkinx

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React wangliang
+            Learn React wangliang1
           </a>
         </header>
       </div>
